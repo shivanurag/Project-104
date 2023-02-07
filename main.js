d943a33c19de1e8246c2595f17933584f7c3817d
shivanurag <77777785+shivanurag@users.noreply.github.com>
2023-02-07 19:04:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set
+({
+    width:300,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+webcam=document.getElementById("webcam");
+Webcam.attach('#webcam');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    })
+}
+
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/efRHm2fsD/model.json',modelLoaded);
+
